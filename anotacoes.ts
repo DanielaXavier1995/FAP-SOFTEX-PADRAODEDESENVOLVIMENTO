0ddd80d689593629107dde21f49af48a5ff37b88
Daniela Xavier <116307469+DanielaXavier1995@users.noreply.github.com>
2023-09-26 18:50:37 +0000

    iniciar typeScript

---
+++ anotacoes.ts
@@ -0,0 +1,18 @@
+console.log("hello world!")
+
+let nome: string = "Dani";
+
+function soma(n1: number, n2: number): number {
+    return n1 + n2
+}
+
+function soma1(n1: number, n2: number): number {
+    return n1 + n2
+}
+
+const soma2 = (n1:number, n2: number): number => {
+    return n1 + n2
+}
+
+let texto: string | number //aceita string e number
+let texto1: any //aceita qualquer tipo
---
+++ main.ts
@@ -0,0 +1,8 @@
+let numero: number = 10
+let valorNulo: null 
+let texto: string = "Ola, mundo"
+let boleano: boolean = true
+let arr: Array<String> = ["ana", "jose", "maria"]
+let obj: Object = {"ana": 12, "jose": 50, "maria": 35}
+
+console.log(texto);
